e9209cdf31c907aeeaf2c47aae19be31fbf26af8
liujiapeng <jiapeng.liu@xinmei365.com>
2016-10-27 12:13:43 +0800

    first commit

---
+++ com/src/kikakeyboard/jenkins/HelloWorld.java
@@ -0,0 +1,10 @@
+package kikakeyboard.jenkins;
+
+/**
+ * Created by xm001 on 16/10/27.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
